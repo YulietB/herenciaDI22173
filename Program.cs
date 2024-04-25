7ef418636e1d06946bed74dd512176fd4d3af5c6
YulietB <162941294+YulietB@users.noreply.github.com>
2024-04-25 11:55:32 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    /*programa de herencia es la caracteristica de pasar atributos y metodos de pasar otros objetos*/
+    //clase mama
+    class operacion
+    {
+        /*guarda los espacios de memoria de 3 valores*/
+        //modificador tipo de dato nombre
+        protected            int          valor1;
+        protected            int          volor2;
+        protected            int          resultado;
+    }
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
